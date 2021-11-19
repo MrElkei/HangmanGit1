c4f223c1af299270d24c8461a96acc030a7f5a92
artishox <44407149+artishox@users.noreply.github.com>
2021-11-19 21:40:10 +0200

    old foldr was added to the .gitignore

--- test.py
+++
@@ -1,7 +0,0 @@
-from scripts.display import Display
-from animations.animationWelcome import AnimationWelcome
-from animations.animationHangman import AnimationHangman
-
-display = Display()
-
-display.animate(left_colums_animation=AnimationHangman(), middle_column_animation=AnimationWelcome())
