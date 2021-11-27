e109bc50ca672a783d5dd5676cad06386047ebc4
MrElkei <lauris.kaukis@gmail.com>
2021-11-27 12:00:34 +0200

    PEP8 linear changes for split_difficulty

--- scripts/split_difficulty.py
+++ scripts/split_difficulty.py
@@ -6,7 +6,8 @@ import os
 """Finds the exact location of the game in your computer"""
 script_dir = os.path.dirname(os.path.realpath(__file__))
 # 2. Pievieno words.txt atrašanās vietu
-"""Adding words.txt file, which contains all the guessable words, location"""
+"""Adding words.txt file, which contains
+all the guessable words, location"""
 words_file = os.path.join(script_dir, '..', 'data', 'words.txt')
 
 # Lasām vārdu sarakstu, kā tekstu
@@ -19,7 +20,8 @@ with open(words_file, 'r', encoding='utf-8') as file:
 words_list = words_str.splitlines()
 
 # print(words_list)
-"""1st difficulty setting calculates word length - the longer the word, the higher the difficulty"""
+"""1st difficulty setting calculates word length - the longer the word,
+the higher the difficulty"""
 word_len = []
 for word in words_list:
     word_len.append(len(word))
@@ -28,8 +30,9 @@ word_len_unique = []
 for word in words_list:
     word_len_unique.append(len(set(word)))
 
-"""2nd difficulty setting calculates the amount of most popular letters for guessing in the word.
-The higher the amount of these letters, the easier the difficulty"""
+"""2nd difficulty setting calculates the amount of most popular letters
+for guessing in the word. The higher the amount of these letters,
+the easier the difficulty"""
 popular_letters = ['A', 'S', 'I', 'E', 'T', 'R', 'N', 'P', 'Ā']
 popular_letters_count = []
 for word in words_list:
@@ -39,7 +42,8 @@ for word in words_list:
             count += 1
     popular_letters_count.append(count)
 
-"""2nd difficulty setting calculates the amount of least popular letters for guessing in the word.
+"""2nd difficulty setting calculates the amount of least popular letters
+for guessing in the word.
 The higher the amount of these letters, the harder the difficulty"""
 unpopular_letters = ['G', 'Ņ', 'Ļ', 'Ģ', 'Ķ', 'Ž', 'H', 'Č']
 unpopular_letters_count = []
@@ -54,7 +58,8 @@ score_list = []
 for n in range(0, len(words_list)):
 
     #Vārda garuma punktu sadale
-    """Score count for word length - for more letters is added higher score, which gives additional points to overall difficulty"""
+    """Score count for word length - for more letters in the word higher
+    score is added, which gives additional points to word difficulty"""
     word_len_score = 0
     if word_len[n] < 7:
         pass
@@ -67,10 +72,12 @@ for n in range(0, len(words_list)):
 
 
     #Unikālo burtu punktu sadale
-    """Unique letter count function. The more the reppetitive letters are in the word, the easier the overall difficulty"""
+    """Unique letter count function. The more the reppetitive letters
+    are in the word, the easier the overall difficulty"""
     letter_repetitiveness_score = 0
     if word_len[n] > 7:
-        letter_repetitiveness = int(((word_len[n] - word_len_unique[n]) * 100) / word_len[n])
+        letter_repetitiveness = (int(((word_len[n] - word_len_unique[n]) * 100)
+         / word_len[n]))
         if letter_repetitiveness < 40:
             letter_repetitiveness_score +=3
         elif letter_repetitiveness < 60:
@@ -83,9 +90,8 @@ for n in range(0, len(words_list)):
     #Nepopulāro burtu punktu sadale
     """Unpopular letter for guessing point count"""
 
-    unpopular_letter_repetitiveness = int((unpopular_letters_count[n] * 100) / word_len[n])
-
-    #print(f'unpopular_letter_repetitiveness: {unpopular_letter_repetitiveness}')
+    unpopular_letter_repetitiveness = (int((unpopular_letters_count[n] * 100) 
+    / word_len[n]))
 
     unpopular_letter_score = 0
     if unpopular_letter_repetitiveness < 10:
@@ -100,9 +106,8 @@ for n in range(0, len(words_list)):
 
     #Populāro burtu punktu sadale
     """Score for popular letter count."""
-    popular_letter_repetitiveness = int((popular_letters_count[n] * 100) / word_len[n])
-
-    #print(f'popular_letter_repetitiveness: {popular_letter_repetitiveness}')
+    popular_letter_repetitiveness = (int((popular_letters_count[n] * 100) 
+    / word_len[n]))
 
     popular_letter_score = 0
     if popular_letter_repetitiveness < 20:
@@ -114,14 +119,17 @@ for n in range(0, len(words_list)):
     else:
         pass
 
-    score = word_len_score + letter_repetitiveness_score + popular_letter_score + unpopular_letter_score
+    score = (word_len_score + letter_repetitiveness_score 
+    + popular_letter_score + unpopular_letter_score)
     score_list.append(score)
 
 print('Score novērtējumu sadalījums:')
 for i in range(1, 12):
     print(f"{i}: {score_list.count(i)}")
 
-"""Function for word list split in 3 difficulties - easy, medium and hard. Also added a chance for user to choose the difficulty of the game"""
+"""Function for word list split in 3 difficulties - easy, medium
+and hard. Also added a chance for user
+to choose the difficulty of the game"""
 easy_words_list = []
 medium_words_list = []
 hard_words_list = []
@@ -141,7 +149,8 @@ easy_words_file = os.path.join(script_dir, '..', 'data', 'easy_words.txt')
 medium_words_file = os.path.join(script_dir, '..', 'data', 'medium_words.txt')
 hard_words_file = os.path.join(script_dir, '..', 'data', 'hard_words.txt')
 
-"""3 different files for difficulties added, which include only words of the needed difficulties in the files with acording name"""
+"""3 different files for difficulties added, which include only words 
+of the needed difficulties in the files with acording name"""
 with open(easy_words_file, 'w', encoding='utf-8') as file:
 	file.write(easy_words)
 
