3f4448a919c552ec2e2af5e97e977051cf2620c2
artishox <44407149+artishox@users.noreply.github.com>
2021-12-04 14:28:22 +0200

    Test started

---
+++ tests/test_display_animations.py
@@ -0,0 +1,12 @@
+import pytest
+
+from scripts.display import Display
+
+def test_display_init():
+    display = Display()
+    assert hasattr(display, "left_column_max")
+    assert hasattr(display, "middle_column_max")
+    assert hasattr(display, "right_column_max")
+    assert hasattr(display, "init_rows")
+    assert hasattr(display, "delim_1")
+    assert hasattr(display, "delim_2")
